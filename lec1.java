e6ae419686043010f3f36450a5c55a4aaab0fe81
ymanish1475-del <ymanish1475@gmail.com>
2025-12-21 23:30:43 +0530

    first commit

---
+++ lec1.java
@@ -0,0 +1,8 @@
+public class lec1 {
+    public static void main(String args[]){
+        System.out.println("hello world with java");
+        System.out.print("hello world with java \n");
+        System.out.println("hello world with java");
+
+    }
+}
