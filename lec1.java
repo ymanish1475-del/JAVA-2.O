b89fc5bb248d07efa1c8813cc0b3aca7e43a5066
ymanish1475-del <ymanish1475@gmail.com>
2025-12-22 22:07:08 +0530

    my java learning

--- lec1.java
+++ lec1.java
@@ -1,7 +1,7 @@
 public class lec1 {
     public static void main(String args[]){
         System.out.println("hello world with java");
-        System.out.print("hello world with java \n");
+        System.out.print("hello world with java \nwith manish project \n");
         System.out.println("hello world with java");
 
     }
