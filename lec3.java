f2059510ff3a564ed8893c17fcbedf9a876e6f31
ymanish1475-del <ymanish1475@gmail.com>
2025-12-24 20:11:18 +0530

    new learning

--- lec3.java
+++ lec3.java
@@ -1,3 +1,17 @@
+import java.util.*;
+
 public class lec3 {
-    
+    public static void main (String[] args){
+        //input
+        Scanner sc = new Scanner (System.in);
+        // String name = sc.nextLine();
+        //nextInt()= int 
+        //nextFloat()
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int man = a+b;
+        // System.out.println(name);
+         System.out.println(man);
+    }
 }
+
