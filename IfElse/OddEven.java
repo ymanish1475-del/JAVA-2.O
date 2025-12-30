bb057e2c8516f4befe425ddb2ae8acf74c351cd7
ymanish1475-del <ymanish1475@gmail.com>
2025-12-30 13:41:55 +0530

    LEARN FINDING A NUM IS EVEN OR ODD

---
+++ IfElse/OddEven.java
@@ -0,0 +1,19 @@
+package IfElse;
+
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String args[]){
+
+        Scanner sc =new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if(x%2==0){
+            System.out.println("ye even number hea ");
+        }else{
+            System.out.println(" ye odd number hea ");
+
+    }
+    
+}
+}
