49a42217937f838cc10701e073ef1a5e983dc347
ymanish1475-del <ymanish1475@gmail.com>
2025-12-30 13:31:25 +0530

    LEARN CONDITION STATEMENT

---
+++ IfElse/condition.java
@@ -0,0 +1,19 @@
+ package IfElse;
+
+ import java.util.*;
+
+public class condition {
+    
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if(age>=18){
+            System.out.println("you are adult ");
+        }else{
+            System.out.println("ypu are not adult ");
+        }
+
+
+    }
+}
